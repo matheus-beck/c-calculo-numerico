bc91b21c6ad385ecbe630838850214e650f04e71
matheus-beck <39171184+matheus-beck@users.noreply.github.com>
2018-06-26 22:55:24 -0300

    Add files via upload

---
+++ metodobs.cc
@@ -0,0 +1,57 @@
+// UNIVERSIDADE FEDERAL DA BAHIA
+// MAT174 - CALCULO NUMÉRICO I
+// Autores: Matheus Beck e Lucio Vinicius
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+double funcao(double x){
+	//Se desejar utilizar outra f(x), alterar essa função
+	return 2.02*pow(x, 5) - 1.28*pow(x, 4) + 3.06*pow(x,3) + 2.92*pow(x,2) - 5.66*x + 6.08;
+}
+
+bool verificaMultiplicacao(double a, double x){
+	double f1 = funcao(a);
+	double f2 = funcao(x);
+	if ((f1 * f2) > 0){
+		return true;
+	}
+	else
+		return false;
+}
+
+//Dados iniciais: intervalo [a, b] e precisão epsilon
+double metodoBissecao(double a, double b, double epsilon){
+	//cout << "Valor de a: " << a << " Valor de b: " << b;
+	//Condição de Parada:
+	if ((fabs(b-a)) < epsilon){ //fabs() retorna o módulo da operação
+
+		cout << endl << "\nFim. Epsilon pertence a: ";
+		cout << a <<" e "<< b << endl;
+		cout << endl << "X = "<< (a+b)/2 << endl; //Raiz encontrada final
+		return 0; 
+	}
+	else {
+		if (verificaMultiplicacao(a,(a+b)/2) == true){
+			a = (a + b)/2;
+			cout << "Valor de X: " << a; //Raiz encontrada
+			cout << " Valor de fabs(x-xbarra): " << fabs(-1.31146 - a) << endl; //Valor do erro absoluto
+			return metodoBissecao(a, b, epsilon);
+		}
+		else {
+			b = (a + b)/2;
+			cout << "Valor de X: " << b; //Raiz encontrada
+			cout << " Valor de fabs(x-xbarra) = " << fabs(-1.31146 - b) << endl; //Valor do erro absoluto
+			return metodoBissecao(a, b, epsilon);
+		}	
+	}
+}
+
+int main(){
+	//O valor real de X é -1.31146 segundo o WolframAlpha
+	metodoBissecao(-2,-1,0.001);
+	return 0;
+}
---
+++ metodonewton.cc
@@ -0,0 +1,42 @@
+// UNIVERSIDADE FEDERAL DA BAHIA
+// MAT174 - CALCULO NUMÉRICO I
+// Autores: Matheus Beck e Lucio Vinicius
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+double funcao(double x){
+	//Se desejar utilizar outra f(x), alterar essa função
+	return 2.02*pow(x, 5) - 1.28*pow(x, 4) + 3.06*pow(x,3) + 2.92*pow(x,2) - 5.66*x + 6.08; //(1)
+}
+
+double funcaoDerivada(double x){
+	//Depende exclusivamente da minha funcao()
+  	return 10.1*pow(x, 4) - 5.12*pow(x, 3) + 9.18*pow(x,2) + 5.84*x - 5.66; //Derivada de (1)
+}
+void metodoNewton(double xi, double epsilon, int qtdMaxLoop){
+	int count;
+	for (count = 0; count < qtdMaxLoop; count++){
+		double x = xi - (funcao(xi)/funcaoDerivada(xi));
+		cout << "k = " << count << "; x = " << x;
+		//Condição de parada:
+		if ((fabs(x-xi) < epsilon) || fabs(x-xi)/fabs(x) < epsilon || fabs(xi) < epsilon){
+			cout << "Raiz encontrada: " << x << "\n";
+			break;
+		}
+		xi = x;
+		//Valor do erro absoluto:
+		cout << " Valor de fabs(x-xbarra): " << fabs(-1.31146 - xi) << endl;
+	}
+}
+
+int main(){
+	//O valor real de X é -1.31146 segundo o WolframAlpha
+	cout << "Após o método de newton: \n";
+	metodoNewton(-1, 0.001, 20);
+
+	return 0;
+}
